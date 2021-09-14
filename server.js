f46b96b944e3dde726b8aff3aa1ea17eae928cbd
yousef-alramli <ysabdys1996@gmail.com>
2021-09-14 20:26:24 +0300

    deployed liveWeather and movies

--- server.js
+++ server.js
@@ -3,33 +3,63 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const axios = require('axios');
 require('dotenv').config();
-const weatherData = require('./data/weather.json')
+const weatherData = require('./data/weather.json');
+const { query } = require('express');
 const PORT = process.env.PORT;
+const movie = process.env.MOVIE_API_KEY;
 
-app.get('/weatherData', (req, res) => {
-    let amman = weatherData[2];
-    let city_name = amman.city_name;
-    let lat = Number(amman.lat);
-    let lon = Number(amman.lon);
-    let latQuery = req.query.lat;
-    let lonQuery = req.query.lon;
-    let searchQuery=req.query.searchQuery.toLowerCase();
-    
-    let city = weatherData.find(item =>{
-       return latQuery == item.lat && lonQuery == item.lon && item.city_name.toLowerCase()==searchQuery;
 
-    })   
-let foreCast= city.data.map(info=>{
-    return{
-        date:info.valid_date ,
-    description:info.weather.description
+
+let handleLiveWeather = async (req, res) => {
+    let lon = req.query.lon
+    let lat = req.query.lat
+    let liveUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`
+    let liveWeather = await axios.get(liveUrl);
+    let LiveData = liveWeather.data.data;
+    let needetData = LiveData.map(item => {
+        let date = item.datetime;
+        let description = item.weather.description;
+        return new foreCast(date, description)
+    });
+    res.status(200).json(needetData);
 }
-})
-res.status(200).send(foreCast);
-});
-
-app.listen(PORT, () => {
-});
 
 
+app.get('/weatherData', handleLiveWeather);
+
+
+
+class foreCast {
+    constructor(date, description) {
+
+        this.date = date,
+            this.description = description
+    }
+};
+
+let handleMovies = async (req, res) => {
+
+
+    let query = req.query.query;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${movie}&query=${query}`;
+    let topMovies = await axios.get(movieUrl);
+    let MoviesData = topMovies.data.results;
+    let wantedMovie = MoviesData.map(item => {
+        let title = item.title;
+        let overview = item.overview;
+        return new foreCastMovies(title, overview)
+    })
+    res.status(200).json(wantedMovie);
+}
+
+app.listen(PORT)
+class foreCastMovies {
+    constructor(title, overview) {
+        this.title = title;
+        this.overview = overview;
+    }
+}
+
+app.get('/movies', handleMovies)
